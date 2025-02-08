d59309e0209bb247eeca1438613bf7f8cbfbb27d
Israel Queiroz <israel.qoliveira@gmail.com>
2025-02-08 20:00:20 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/queirozisrael/springmongodb/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.queirozisrael.springmongodb.repositories;
+
+import com.queirozisrael.springmongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
