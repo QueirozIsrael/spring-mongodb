d84ec329526c6c6a3949e0d1dda4d27709dfaecc
Israel Queiroz <israel.qoliveira@gmail.com>
2025-02-09 00:54:57 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/queirozisrael/springmongodb/resources/PostResource.java
+++ src/main/java/com/queirozisrael/springmongodb/resources/PostResource.java
@@ -1,18 +1,13 @@
 package com.queirozisrael.springmongodb.resources;
 
 import com.queirozisrael.springmongodb.domain.Post;
-import com.queirozisrael.springmongodb.domain.User;
-import com.queirozisrael.springmongodb.dto.UserDTO;
 import com.queirozisrael.springmongodb.services.PostService;
-import com.queirozisrael.springmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value ="/posts")
