e5920368318a19b65ce6d25a5aa50ab1fdf9f083
Israel Queiroz <israel.qoliveira@gmail.com>
2025-02-09 00:45:37 -0300

    Obtendo um post por id

---
+++ src/main/java/com/queirozisrael/springmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.queirozisrael.springmongodb.services;
+
+import com.queirozisrael.springmongodb.domain.Post;
+import com.queirozisrael.springmongodb.repositories.PostRepository;
+import com.queirozisrael.springmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
